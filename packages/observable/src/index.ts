54cb3656803df25e79824820aad97cb60cb65187
liaoyinglong <vigossliao@gmail.com>
2021-01-13 16:46:55 +0800

    feat(observable): export steams

--- packages/observable/src/index.ts
+++ packages/observable/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./creator/createNotify";
 export * from "./creator/intervalOnDocumentShow";
 export * from "./creator/subscribeWhenDocumentShow";
+export * from "./steams/documentVisible$";
