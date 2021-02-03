96af210e2b8775a9bc1b89f947f7682012eec824
liaoyinglong <vigossliao@gmail.com>
2021-02-03 16:18:37 +0800

    :sparkles: 添加新的方法

---
+++ packages/utils/src/CrossPlatformMethods/index.ts
@@ -0,0 +1,56 @@
+import { ApiEnvType } from "../ApiEnv";
+import { wrapperedQs } from "../wrapperedQs";
+import jsCookies from "js-cookie";
+import { AppMethods } from "../AppMethods";
+
+function getUserTokenFromCookie(ApiEnv: ApiEnvType) {
+  let key = "userToken";
+  if (ApiEnv.isTestNet) {
+    key = "testUserToken";
+  }
+  if (ApiEnv.isDev) {
+    key = "devUserToken";
+  }
+  return jsCookies.get(key);
+}
+
+function parseUrlToken() {
+  const parsed = wrapperedQs.parse(window.location.search);
+  return parsed.token || parsed.userToken;
+}
+
+interface CreateCrossPlatformMethodsParams {
+  ApiEnv: ApiEnvType;
+}
+
+export function createCrossPlatformMethods({
+  ApiEnv,
+}: CreateCrossPlatformMethodsParams) {
+  return class CrossPlatformMethods {
+    /**
+     * 获取token 顺序
+     * 1. url上获取
+     * 2. app 内打开使用 app方法获取
+     * 3. 从cookie上获取
+     */
+    static getUserToken() {
+      const urlToken = parseUrlToken();
+      if (urlToken) {
+        return urlToken;
+      }
+      if (AppMethods.isInAppWebView) {
+        return AppMethods.getUserToken();
+      }
+      return getUserTokenFromCookie(ApiEnv);
+    }
+
+    // TODO: 需要配置完成 urlMaps
+    static goLogin() {
+      if (AppMethods.isInAppWebView) {
+        AppMethods.goLogin();
+        return;
+      }
+      return;
+    }
+  };
+}
