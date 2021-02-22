6d377e641d76ccd152319a46daa1010b5c06de81
liaoyinglong <vigossliao@gmail.com>
2021-02-22 15:24:08 +0800

    :construction: Work in progress.

--- packages/utils/src/urlMaps/index.ts
+++ packages/utils/src/urlMaps/index.ts
@@ -1,15 +1,59 @@
+import { BehaviorSubject } from "rxjs";
+import { ApiEnvType } from "../ApiEnv";
+import _ from "lodash";
+import { normalizeLink } from "../normalizeLink";
+
 interface CreateUrlHelpersParams {
-
-  ApiEnv: {
-    current: string;
-    isDev: boolean;
-    isPro: boolean;
-    isTestNet: boolean;
-  };
-  // 获取
-  getDomain:()=>string
-
-
+  ApiEnv: ApiEnvType;
+  // 当前可用的域名
+  domainNameSubject: BehaviorSubject<string>;
 }
 
-export function createUrl() {}
+export function createUrl(params: CreateUrlHelpersParams) {
+  const { ApiEnv, domainNameSubject } = params;
+  // 生成接口请求的url前缀
+  function getServiceUrl(projectName: string, urlPrefix = ApiEnv.urlEnvPrefix) {
+    return `https://${urlPrefix}${projectName}.${domainNameSubject.getValue()}`;
+  }
+  // 获取活动页h5的链接
+  function getH5UrlCreator(p: string) {
+    return (query: Record<string, any>) => {
+      if (!_.startsWith(p, "/")) {
+        p = "/" + p;
+      }
+      const url = `https://${
+        ApiEnv.urlEnvPrefix
+      }h5.${domainNameSubject.getValue()}${p}`;
+
+      return normalizeLink(url, query);
+    };
+  }
+
+  return {
+    get oc() {
+      return getServiceUrl("oc");
+    },
+    get api() {
+      return getServiceUrl("api");
+    },
+    get news() {
+      // testnet 没有 快讯api  使用 正式环境的
+      return getServiceUrl(
+        "news",
+        ApiEnv.isTestNet ? ApiEnv.urlEnvPrefixMap.pro : ApiEnv.urlEnvPrefix
+      );
+    },
+    get user() {
+      return getServiceUrl("user");
+    },
+    get cbbcapi() {
+      return getServiceUrl("cbbcapi");
+    },
+    get otc() {
+      return getServiceUrl("otc");
+    },
+    get facade() {
+      return getServiceUrl("facadeapi");
+    },
+  };
+}
