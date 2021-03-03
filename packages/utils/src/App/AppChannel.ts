406838d29fcf43b382a25c7ee28529f980033820
liaoyinglong <vigossliao@gmail.com>
2021-03-03 10:57:16 +0800

    :sparkles: 添加 appchannel

---
+++ packages/utils/src/App/AppChannel.ts
@@ -0,0 +1,9 @@
+export const AppChannel = {
+  android_beta: "android_beta",
+  android_hopex: "android_hopex",
+  android_google: "android_google",
+
+  ios_hopex: "ios_hopex",
+  ios_appstore: "ios_appstore",
+  ios_tf: "ios_tf",
+};
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,7 +1,8 @@
-export { AppMethods } from "./AppMethods";
+export { AppChannel } from "./App/AppChannel";
+export { AppMethods } from "./App/AppMethods";
 export { Bar, normalizeKLinesWithAction } from "./normalizeKLinesWithAction";
-export { WsService } from "./WsService";
 export { createApiEnv, ApiEnvType } from "./ApiEnv";
 export { enableLogger, createLogger } from "./logger";
 export { isDeepEqual } from "./isDeepEqual";
 export { wrapperedQs } from "./wrapperedQs";
+export { WsService } from "./WsService";
