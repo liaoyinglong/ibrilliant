5155920c3c6e3fa3e2b571da706164e3cb425833
liaoyinglong <vigossliao@gmail.com>
2021-03-03 17:53:38 +0800

    :sparkles: 支持debug配置

--- packages/hooks/src/makeSlice/index.ts
+++ packages/hooks/src/makeSlice/index.ts
@@ -2,13 +2,15 @@ import { useMemo, useReducer } from "react";
 import type { CreateSliceOptions, SliceCaseReducers } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
 import { bindActionCreators } from "redux";
+import { createLogger } from "@ibrilliant/utils";
+import _ from "lodash";
 
 /**
  * @description 返回对`useReducer`进行封装的hooks 内部使用 `@reduxjs/toolkit`的`slice`来处理
  * @param options @reduxjs/toolkit createSlice 的配置项
  * @example
  * ```js
-const useSlice = makeSlice({
+ const useSlice = makeSlice({
   name: "name是为了debug用的",
   initialState: { count: 0 },
   reducers: {
@@ -29,8 +31,14 @@ export function makeSlice<
   State,
   CaseReducers extends SliceCaseReducers<State>,
   Name extends string = string
->(options: CreateSliceOptions<State, CaseReducers, Name>) {
+>(
+  options: CreateSliceOptions<State, CaseReducers, Name> & { debug?: boolean }
+) {
   const slice = createSlice(options);
+
+  const log = options.debug
+    ? createLogger(`${options.name}:debug` || "makeSlice:debug")
+    : _.noop;
 
   return function useSlice(initialState = options.initialState) {
     const [state, dispatch] = useReducer(slice.reducer, initialState);
@@ -40,7 +48,13 @@ export function makeSlice<
         // TODO: fix type error
         // @ts-ignore
         slice.actions,
-        dispatch
+
+        options.debug
+          ? (action) => {
+              log(`type:${action.type},payload:%o`, action.payload);
+              dispatch(action);
+            }
+          : dispatch
       ) as unknown) as typeof slice["actions"];
     }, []);
 
