402e6cf6ebe32b214b41a6ea9c322c9e14e0eda2
liaoyinglong <vigossliao@gmail.com>
2021-03-08 11:40:30 +0800

    :sparkles: Introducing new features.

--- packages/hooks/src/Observable/useSubject/index.ts
+++ packages/hooks/src/Observable/useSubject/index.ts
@@ -1,6 +1,17 @@
 import { Subject } from "rxjs";
 import { useConstant } from "../../useConstant";
 
+/**
+ * 这个hooks返回的subject做了bind this 的处理
+ * 目的是为了在 jsx 里面使用起来更方便
+ */
 export function useSubject<T>() {
-  return useConstant(() => new Subject<T>());
+  return useConstant(() => {
+    const subject = new Subject<T>();
+    subject.next = subject.next.bind(subject);
+    subject.error = subject.error.bind(subject);
+    subject.complete = subject.complete.bind(subject);
+    subject.unsubscribe = subject.unsubscribe.bind(subject);
+    return subject;
+  });
 }
