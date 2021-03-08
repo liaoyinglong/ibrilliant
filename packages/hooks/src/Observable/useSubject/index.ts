97af371facd32a70722f18890b2a37477b337bec
liaoyinglong <vigossliao@gmail.com>
2021-03-08 11:25:50 +0800

    :recycle: Refactoring code.

--- packages/hooks/src/Observable/useSubject/index.ts
+++ packages/hooks/src/Observable/useSubject/index.ts
@@ -1,5 +1,5 @@
 import { Subject } from "rxjs";
-import { useConstant } from "../useConstant";
+import { useConstant } from "../../useConstant";
 
 export function useSubject<T>() {
   return useConstant(() => new Subject<T>());
--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -8,13 +8,14 @@ export {
 export { createProviderAndHooks } from "./createProviderAndHooks";
 export { makeSlice } from "./makeSlice";
 export { createSliceStateHook, useSliceState } from "./createSliceHook";
-export { useBehaviorSubjectControl } from "./useBehaviorSubjectControl";
+export { useBehaviorSubjectControl } from "./Observable/useBehaviorSubjectControl";
 export { useConstant } from "./useConstant";
 export { useLocationQuery } from "./useLocationQuery";
 export { useMuiInputControl } from "./useMuiInputControl";
 export { useMuiTabsControl } from "./useMuiTabsControl";
 export { useSelectorWithIsEqual } from "./useSelectorWithIsEqual";
-export { useSubject } from "./useSubject";
+export { useSubject } from "./Observable/useSubject";
+export { useSubscribe } from "./Observable/useSubscribe";
 
 export {
   NumberInputProps,
