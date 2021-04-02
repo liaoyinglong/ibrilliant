a4dbe891f1fceefb695c1e1561d4009a0bc4a38a
liaoyinglong <vigossliao@gmail.com>
2021-04-02 14:13:14 +0800

    :bug: Fixing a bug.

--- packages/umi-preset/src/plugins/rewriteFastRefresh.ts
+++ packages/umi-preset/src/plugins/rewriteFastRefresh.ts
@@ -1,6 +1,9 @@
 import type { IApi } from "umi";
 
 export default function rewriteFastRefresh(api: IApi) {
+  if (api.env !== "development") {
+    return;
+  }
   api.chainWebpack((config) => {
     config.plugin("fastRefresh").tap(([options]) => {
       return [
