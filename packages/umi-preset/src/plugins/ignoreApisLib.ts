e923e7c5282c3f5ff6a7b24c51aabfce2af95f84
liaoyinglong <vigossliao@gmail.com>
2021-04-02 11:23:10 +0800

    :mute: Removing logs.

--- packages/umi-preset/src/plugins/ignoreApisLib.ts
+++ packages/umi-preset/src/plugins/ignoreApisLib.ts
@@ -5,7 +5,6 @@ export default function ignoreApisLib(api: IApi) {
     config.module.rule("js").exclude.add((p: string) => {
       // sw2rx 工具会生成es5的代码
       if (p.includes("apis/lib") || p.includes("apis\\lib")) {
-        console.log("需要忽略", p);
         return true;
       }
       return false;
