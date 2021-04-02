47dbd236ba6748199fcd68d2ff2d60b74a22283a
liaoyinglong <vigossliao@gmail.com>
2021-04-02 13:45:28 +0800

    :recycle: Refactoring code.

--- packages/umi-preset/src/index.ts
+++ packages/umi-preset/src/index.ts
@@ -2,12 +2,10 @@ export default () => {
   return {
     plugins: [
       require.resolve("./plugins/sass"),
+      require.resolve("./plugins/css"),
       require.resolve("./plugins/rewriteFastRefresh"),
       require.resolve("./plugins/ignoreApisLib"),
       require.resolve("./plugins/emotion"),
-      require.resolve("./plugins/rewriteExtractCssLoader"),
-      require.resolve("./plugins/rewritePostcssLoader"),
-      require.resolve("./plugins/updateCssModulesQuery"),
       require.resolve("./plugins/webpack5"),
       require.resolve("./plugins/applyEsbuildInDev"),
     ],
---
+++ packages/umi-preset/src/plugins/css.ts
@@ -0,0 +1,34 @@
+import { IApi } from "umi";
+
+export default function css(api: IApi) {
+  api.chainWebpack((config) => {
+    [
+      config.module.rule("css").oneOf("css"),
+      config.module.rule("css").oneOf("css-modules"),
+      config.module.rule("less").oneOf("css"),
+      config.module.rule("less").oneOf("css-modules"),
+      config.module.rule("sass").oneOf("css"),
+      config.module.rule("sass").oneOf("css-modules"),
+    ].forEach((item) => {
+      item
+        .use("postcss-loader")
+        .clear()
+        .loader(require.resolve("postcss-loader"));
+
+      item.use("extract-css-loader").options({
+        publicPath: api.userConfig.publicPath,
+        hmr: api.env === "development",
+      });
+    });
+
+    [
+      config.module.rule("css").oneOf("css-modules"),
+      config.module.rule("less").oneOf("css-modules"),
+      config.module.rule("sass").oneOf("css-modules"),
+    ].forEach((item) => {
+      item.delete("resourceQuery");
+    });
+
+    return config;
+  });
+}
